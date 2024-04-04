f56cd2c4af3ceee2813415e31e689125fe489e32
jlnikhilrao <nikhilraojl@outlook.com>
2024-04-04 14:47:31 +0530

    Add option to show more than 1 future GPs

--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,37 @@ fn run() -> Result<()> {
     args.next();
 
     if let Some(arg) = args.next() {
-        if arg == "next" {
-            let curr_dt = Local::now();
-            let mut output = String::new();
+        match arg.as_ref() {
+            "next" => {
+                let mut num_to_show: u8 = if let Some(arg) = args.next() {
+                    arg.parse()?
+                } else {
+                    1
+                };
+                let curr_dt = Local::now();
+                let mut output = String::new();
 
-            for race in race_schedule(false)?.races {
-                if curr_dt < race.gp_start_dt() {
-                    race.pp_race_name(&mut output)?;
-                    break;
+                for race in race_schedule(false)?.races {
+                    if curr_dt < race.gp_start_dt() {
+                        race.pp_race_name(&mut output)?;
+                        num_to_show -= 1;
+                        if num_to_show == 0 {
+                            break;
+                        }
+                    }
+                }
+                println!("{output}");
+            }
+            "standings" => {
+                for driver in driver_standings(false)? {
+                    println!("{:<20} {}", driver.name, driver.points)
                 }
             }
-            println!("{output}");
-        } else if arg == "standings" {
-            for driver in driver_standings(false)? {
-                println!("{:<20} {}", driver.name, driver.points)
+            "pull" => {
+                race_schedule(true)?;
+                driver_standings(true)?;
             }
-        } else if arg == "pull" {
-            race_schedule(true)?;
-            driver_standings(true)?;
+            _ => {eprintln!("Not a valid command")}
         }
     }
     Ok(())
